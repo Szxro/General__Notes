9c1d5cfca3900636daaf4c80b317561ff93a5dd7
Szxro <szxrocode@gmail.com>
2022-12-05 13:43:45 -0400

    new notes

--- CSS/practices/Media Queries/js/index.js
+++
@@ -1,7 +0,0 @@
-window.onresize = screen;
-window.onload = screen;
-
-function screen() {
-  let width = window.innerWidth;
-  document.getElementById("size").innerHTML = "Width: " + width + "px";
-}
--- CSS/practices/Navbar/responsive/Navbar/js/index.js
+++
@@ -1,7 +0,0 @@
-function toggle() {
-  const toggle = document.getElementById("toggle__button");
-  const links = document.getElementById("nav__list");
-  if (toggle.click) {
-    links.classList.toggle("active");
-  }
-}
